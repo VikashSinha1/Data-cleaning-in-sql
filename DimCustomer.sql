952d023298ab4925d5d276607ccc1919e6493416
VikashSinha1 <93470035+VikashSinha1@users.noreply.github.com>
2023-02-21 19:45:56 +0530

    Add files via upload

---
+++ DimCustomer.sql
@@ -0,0 +1,37 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+  c.[CustomerKey] --,[GeographyKey]
+  --,[CustomerAlternateKey]
+  --,[Title]
+  , 
+  c.[FirstName] --,[MiddleName]
+  , 
+  c.[LastName], 
+  c.FirstName + ' ' + c.LastName as 'Full_Name' --,[NameStyle]
+  , 
+  c.[BirthDate] --,[MaritalStatus]
+  --,[Suffix]
+  , 
+  case gender when 'M' THEN 'Male' else 'Female' end as 'Gender' --,[EmailAddress]
+  --,[YearlyIncome]
+  --,[TotalChildren]
+  --,[NumberChildrenAtHome]
+  --,[EnglishEducation]
+  --,[SpanishEducation]
+  -- ,[FrenchEducation]
+  --,[EnglishOccupation]
+  -- ,[SpanishOccupation]
+  -- ,[FrenchOccupation]
+  --,[HouseOwnerFlag]
+  --,[NumberCarsOwned]
+  -- ,[AddressLine1]
+  -- ,[AddressLine2]
+  -- ,[Phone]
+  , 
+  c.[DateFirstPurchase], 
+  g.City --,[CommuteDistance],
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] c 
+  join dbo.DimGeography g on c.GeographyKey = g.GeographyKey --joined geography table to extract city out of it
+order by 
+  CustomerKey
---
+++ dim_products.sql
@@ -0,0 +1,36 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+  p.[ProductKey], 
+  pc.EnglishProductCategoryName as 'product_category' --,p.[ProductSubcategoryKey]
+  , 
+  ps.EnglishProductSubcategoryName as 'product_sub_category' --,[WeightUnitMeasureCode]
+  --,[SizeUnitMeasureCode]
+  , 
+  p.[EnglishProductName] --,[SpanishProductName]
+  --,[FrenchProductName]
+  --,[StandardCost]
+  , 
+  p.[FinishedGoodsFlag], 
+  p.[Color], 
+  p.[SafetyStockLevel], 
+  p.[ReorderPoint], 
+  p.[ListPrice], 
+  p.[Size] as product_size --,[SizeRange]
+  , 
+  p.[Weight] as product_weight, 
+  p.[DaysToManufacture] --,[ProductLine]
+  --,[DealerPrice]
+  --,[Class]
+  --,[Style]
+  --,[ModelName]
+  --,[LargePhoto]
+  --,[StartDate]
+  --,[EndDate]
+  , 
+  isnull(Status,'Outdated')as status
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] as p 
+  join [AdventureWorksDW2019].[dbo].[DimProductSubCategory] as ps on p.ProductSubcategoryKey = ps.ProductSubcategoryKey 
+  join [AdventureWorksDW2019].[dbo].[DimProductCategory] as pc on ps.ProductCategoryKey = pc.ProductCategoryKey 
+order by 
+  p.ProductAlternateKey asc
